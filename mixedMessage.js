0777720b53f07318bcaec15359ddd6f66b23f940
Lauren Callaway <lcallaway14@hotmail.com>
2021-02-02 21:06:55 +0000

    clean up and add comments

--- mixedMessage.js
+++ mixedMessage.js
@@ -3,7 +3,7 @@ const arr1=['Your hair','Your face','Your smile','Your eyes','Your spirit','Your
 const arr2=['dances like','glints like','lights up','moves','inspires','fascinates','ignites','touches'];
 const arr3=['the moon against a dark sky','a candle in the breeze','deeper than the oceans','even the hightest mountains','my heart','the immeasurable wonders of the cosmos','the dreams of others','love itself'];
 
-
+//function to generate nonsense compliments using random words from arrays 
 function generateMessage(){
     var v1 = Math.floor(Math.random()*8);
     var v2 = Math.floor(Math.random()*8);
@@ -11,4 +11,3 @@ function generateMessage(){
     const string = `${arr1[v1]} ${arr2[v2]} ${arr3[v3]}`
     return string;  
 }
-//console.log(generateMessage());
